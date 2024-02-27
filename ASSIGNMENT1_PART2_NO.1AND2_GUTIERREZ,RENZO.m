b627ebc934819d68ce2c661784b7461610705680
rRizu <159804520+rRizu@users.noreply.github.com>
2024-02-27 14:06:23 +0800

    Add files via upload

---
+++ ASSIGNMENT1_PART2_NO.1AND2_GUTIERREZ,RENZO.m
@@ -0,0 +1,18 @@
+% Clear
+clear
+clc
+close all
+
+syms t s
+
+% Inverse Laplace
+
+F1 = (8 - 3*5 + s^2) / s^3;
+f1 = ilaplace (F1);
+pretty (f1)
+
+F2 = (5/s - 2/5^2) - (4*s/(s^2 + 9)) ;
+f2 = ilaplace(F2);
+pretty (f2)
+
+
